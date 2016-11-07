82a0677209adff3b0d25ddfd090b25601f0e5a44
Lintang Timur <lintangtimur@protonmail.com>
2016-11-08 00:17:14 +0700

    Edit, Delete

--- 8november/index.php
+++ 8november/index.php
@@ -8,26 +8,50 @@
     <link rel="stylesheet" href="assets/css/bootstrap.css">
   </head>
   <body>
-    <style type="text/css">
+    <!--Github : github.com/lintangtimur-->
 
+    <style type="text/css">
+    .display-none{
+      display: none;
+    }
     </style>
     <div class="container">
       <div class="page-header">
         <h1>Input KTP</h1>
+        <h4>Hasil liat dibawah FORM</h4>
       </div>
       <?php
+      $kolom = array();
+      $sql_show_column = "show columns from ktp";
+      $res = $mysqli->query($sql_show_column);
+      while($row = $res->fetch_object()){
+        $kolom[] = $row->Field;
+      }
+
+      //Cek Apakah akan ada update
       if(isset($_GET['nik']) && isset($_GET['kode_camat'])){
         $nik = $_GET['nik'];
         $kode_camat = $_GET['kode_camat'];
-        $sql = "select * from ktp,kecamatan where ktp.nik='$nik' and ktp.kode_kecamatan='$kode_camat'";
+        $sql = "select * from ktp,kecamatan where ktp.nik='$nik' and kecamatan.kode_kecamatan='$kode_camat'";
         $result = $mysqli->query($sql);
+
+        //menyimpan variable ke dalam array
         while($row = $result->fetch_object()){
-          $nik =$row->nik;
-          $camat = $row->kode_kecamatan;
+          foreach ($kolom as $var) {
+            $data[] = array($var => $row->$var);
+          }
         }
+        //print_r($data); //Ngecek Data bentuk Array
+        $ro = 'readonly';
+      }else{//Jika tidak update
+        $ro = null;
+        $data = null;
+      }
+
+      if(isset($_GET['edit']) && $_GET['edit']==='true'){
+        $value = "UPDATE";
       }else{
-        $nik = null;
-        $camat = null;
+        $value = "SUBMIT";
       }
        ?>
       <!--Konten-->
@@ -35,39 +59,39 @@
         <table class="table table-bordered table-responsive table-hover">
           <tr>
             <td>NIK</td>
-            <td><input class='form-control' type="text" maxlength="16" name="nik" value="<?php echo $nik;?>"></td>
+            <td><input  class='form-control' type="text" maxlength="16" name="nik" <?php echo $ro;?> value="<?php echo $data[0]["nik"];?>"></td>
           </tr>
           <tr>
             <td>NAMA</td>
-            <td><input type="text" name="nama" class='form-control'></td>
+            <td><input type="text" name="nama" class='form-control' value="<?php echo $data[1]["nama"];?>"></td>
           </tr>
           <tr>
             <td>Tempat Lahir</td>
-            <td><input type="text" name="tempat_lahir" class='form-control'></td>
+            <td><input type="text" name="tempat_lahir" class='form-control' value="<?php echo $data[2]["tempat_lahir"];?>"></td>
           </tr>
           <tr>
             <td>Tanggal Lahir</td>
-            <td><input type="date" name="tgl_lahir" class='form-control'></td>
+            <td><input type="date" name="tgl_lahir" class='form-control' value="<?php echo $data[3]["tgl_lahir"];?>"></td>
           </tr>
           <tr>
             <td>Jenis Kelamin</td>
-            <td><input type="text" name="kelamin" class='form-control'></td>
+            <td><input type="text" name="kelamin" class='form-control' value="<?php echo $data[4]["jenis_kelamin"];?>"></td>
           </tr>
           <tr>
             <td>Golongan Darah</td>
-            <td><input type="text" name="goldar" class='form-control'></td>
+            <td><input type="text" name="goldar" class='form-control' value="<?php echo $data[5]["golongan_darah"];?>"></td>
           </tr>
           <tr>
             <td>Alamat</td>
-            <td><input type="text" name="alamat" class='form-control'></td>
+            <td><input type="text" name="alamat" class='form-control' value="<?php echo $data[6]["alamat"];?>"></td>
           </tr>
           <tr>
             <td>RT RW</td>
-            <td><input type="text" name="rtrw" class='form-control'></td>
+            <td><input type="text" name="rtrw" class='form-control' value="<?php echo $data[7]["rtrw"];?>"></td>
           </tr>
           <tr>
             <td>Kelurahan Desa</td>
-            <td><input type="text" name="keldes" class='form-control'></td>
+            <td><input type="text" name="keldes" class='form-control' value="<?php echo $data[8]["keldes"];?>"></td>
           </tr>
           <tr>
             <td>Kode Kecamatan</td>
@@ -77,8 +101,8 @@
               $sql = 'select * from kecamatan';
               $result = $mysqli->query($sql);
               while($row = $result->fetch_object()){
-                if($camat === $row->kode_kecamatan){
-                  echo "<option value='$row->kode_kecamatan' selected>$row->kode_kecamatan</option>";
+                if($data[9]["kode_kecamatan"] === $row->kode_kecamatan){
+                  echo "<option value='$row->kode_kecamatan' selected>$row->kode_kecamatan.$row->nama_kecamatan</option>";
                 }else{
                   echo "<option value='$row->kode_kecamatan'>$row->kode_kecamatan</option>";
                 }
@@ -88,60 +112,42 @@
           </tr>
           <tr>
             <td>Agama</td>
-            <td><input type="text" name="agama" class='form-control'></td>
+            <td><input type="text" name="agama" class='form-control' value="<?php echo $data[10]["agama"];?>"></td>
           </tr>
           <tr>
             <td>Status Kawin</td>
-            <td><input type="text" name="stkawin" class='form-control'></td>
+            <td><input type="text" name="stkawin" class='form-control' value="<?php echo $data[11]["statuskawin"];?>"></td>
           </tr>
           <tr>
             <td>Pekerjaan</td>
-            <td><input type="text" name="pekerjaan" class='form-control'></td>
+            <td><input type="text" name="pekerjaan" class='form-control' value="<?php echo $data[12]["pekerjaan"];?>"></td>
           </tr>
           <tr>
             <td>KWN</td>
-            <td><input type="text" name="kwn" class='form-control'></td>
+            <td><input type="text" name="kwn" class='form-control' value="<?php echo $data[13]["kwn"];?>"></td>
           </tr>
           <tr>
             <td>Berlaku</td>
-            <td><input type="date" name="berlaku" class='form-control'></td>
+            <td><input type="date" name="berlaku" class='form-control' value="<?php echo $data[14]["berlaku"];?>"></td>
           </tr>
           <tr>
             <td>Tanggal Ditandatangani</td>
-            <td><input type="date" name="ttd" class='form-control'></td>
+            <td><input type="date" name="ttd" class='form-control' value="<?php echo $data[15]["tgl_ditandatangani"];?>"></td>
           </tr>
-
+          <input type="hidden" name="hidden" value="<?php echo $value;?>">
           <tr>
             <td></td>
-            <td><button type="submit" class="btn btn-success">ADD</button></td>
+            <td><button type="submit" class="btn btn-success" name="submit"><?php echo $value;?></button></td>
           </tr>
         </table>
       </form>
     </div>
 
-<div class="container">
+<div class="container-fluid">
   <div id="disini" class="table-responsive"></div>
 </div>
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.js"></script>
-    <script>
-    $(document).ready(function(){
-      $('#formKTP').submit(function(event){
-        event.preventDefault();
-        $.ajax({
-          type: 'POST',
-          url : 'test.php',
-          data : $('#formKTP').serialize()
-        })
-        .done(function(data){
-          console.log(data);
-          $('#disini').html(data);
-        })
-        .fail(function(data){
-          console.log(data);
-        });
-      })
-    })
-    </script>
+    <script src="assets/js/main.js"></script>
   </body>
 </html>
--- 8november/test.php
+++ 8november/test.php
@@ -3,6 +3,7 @@
   DRY => Dont Repeat Yourself
 */
 include 'config/connection.php';
+include 'config/table_config.php';
 $sql = "show columns from ktp";
 $result = $mysqli->query($sql);
 $kolom = array();
@@ -12,55 +13,54 @@ while($row = $result->fetch_object()){
 }
 
 /*GET INPUT FROM FORM*/
+// foreach ($_POST as $key => $value) {
+//   $data[] = "'".$value."'";
+// }
+
 foreach ($_POST as $key => $value) {
-  $data[] = "'".$value."'";
+    $data2[] = "'".$value."'";
 }
+for ($i=0; $i <count($data2)-1 ; $i++) {
+  $data3[] = $data2[$i];
+}
+
 $myKol = implode(",", array_values($kolom));
-$myData = implode(",",array_values($data));
+$myData = implode(",",array_values($data3));
 
-function insertTable($mysqli, $mykol, $mydata){
-  $sql = "INSERT INTO ktp ($mykol) VALUES($mydata)";
-  $result = $mysqli->query($sql);
-  if($result){}else{echo $mysqli->error;}
-}
-function showTable($mysqli,$kolom){
-  $sql = "SELECT * FROM ktp, kecamatan WHERE ktp.kode_kecamatan=kecamatan.kode_kecamatan";
-  $result = $mysqli->query($sql);
-  echo "<table class='table table-responsive table-hover'>";
-  print_header_table($kolom);
-  $i=0;
-  while($row = $result->fetch_object()){
-    echo "<tr>";
-    $i++;
-    echo "<td>$i</td>";
-    foreach ($kolom as $var) {
-      if($var==='kode_kecamatan'){
-        echo "<td>";
-        echo $row->nama_kecamatan;
-        echo "</td>";
-      }else{
-        echo "<td>";
-        echo $row->$var;
-        echo "</td>";
-      }
-    }
-    echo "</tr>";
-  }
-  echo "</table>";
-}
-function print_header_table($kolom){
-  echo "<tr><td>NO</td>";
-  foreach ($kolom as $var) {
-    if($var==='kode_kecamatan'){
-      echo "<td><b>KECAMATAN</b></td>";
-    }else{
-      $th =strtoupper($var);
-      echo "<td><b>$th</b></td>";
-    }
-  }
-  echo "<td>OPTION</></tr>";
+
+
+function updateTable($mysqli,$kolom,$data3){
+  $sql = "UPDATE ktp SET  $kolom[1]=$data3[1],
+                          $kolom[2]=$data3[2],$kolom[3]=$data3[3],
+                          $kolom[4]=$data3[4],$kolom[5]=$data3[5],
+                          $kolom[6]=$data3[6],$kolom[7]=$data3[7],
+                          $kolom[8]=$data3[8],$kolom[9]=$data3[9],
+                          $kolom[10]=$data3[10],$kolom[11]=$data3[11],
+                          $kolom[12]=$data3[12],$kolom[13]=$data3[13],
+                          $kolom[14]=$data3[14],$kolom[15]=$data3[15]
+                    WHERE nik=$data3[0]";
+  $result = $mysqli->query($sql) or die($mysqli->error);
+  showTable($mysqli, $kolom);
 }
 
-insertTable($mysqli, $myKol, $myData);
-showTable($mysqli, $kolom);
+function insertTable($mysqli, $myKol, $myData){
+  $sql = "INSERT INTO ktp ($myKol) VALUES($myData)";
+  // echo $sql;
+  $result = $mysqli->query($sql);
+  if($result){}else{echo "ERROR".$mysqli->error;}
+}
+//Awal mula letak showTable dan printHeader
+
+if($_POST['hidden']==="UPDATE"){
+  updateTable($mysqli, $kolom, $data3);
+}
+else{
+  insertTable($mysqli, $myKol, $myData);
+  showTable($mysqli, $kolom);
+}
+
+/* DEBUG */
+//insertTable($mysqli, $myKol, $myData);
+//showTable($mysqli, $kolom);
+//updateTable($mysqli, $kolom, $data)
  ?>
